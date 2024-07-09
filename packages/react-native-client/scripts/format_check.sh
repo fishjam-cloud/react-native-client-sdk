c7a33f246fcb3bb11276139b0c4ad9bc62d0c974
Karol Kąkol <kakol.karol00@gmail.com>
2024-07-09 12:33:07 +0200

    adjust linting and formating

--- packages/react-native-client/scripts/format_check.sh
+++ packages/react-native-client/scripts/format_check.sh
@@ -1,5 +1,11 @@
 echo "Running format:check for react-native javascript files \n"
 prettier --check . --ignore-path ./.eslintignore
+
+echo "Running format:check for react-native android files \n"
+cd android
+ktlint **/*.kt
+cd ..
+
 echo "Running format:check for react-native ios files \n"
 cd ios
-swift-format format -r ./*.swift --configuration swift-format-config.json
+swift-format lint -r -s ./*.swift --configuration swift-format-config.json
--- packages/react-native-client/scripts/lint.sh
+++ packages/react-native-client/scripts/lint.sh
@@ -1,9 +1,2 @@
 echo "Running lint for react-native javascript files \n"
 eslint . --ext .ts,.tsx --fix
-echo "Running lint for react-native android files \n"
-cd android
-ktlint -F **/*.kt
-cd ..
-echo "Running lint for react-native ios files \n"
-cd ios
-swift-format lint -r -s ./*.swift --configuration swift-format-config.json
