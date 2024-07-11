c51a034f9f7d866aa70eb20b0aa46a611ccbff5d
Miron Pawlik <miron.pawlik@swmansion.com>
2024-07-11 14:55:17 +0200

    Fail on error in lint script

--- packages/react-native-client/scripts/lint.sh
+++ packages/react-native-client/scripts/lint.sh
@@ -1,15 +1,15 @@
 echo "Running eslint for react-native javascript files \n"
-eslint . --ext .ts,.tsx --fix
+eslint . --ext .ts,.tsx --fix || exit 1
 
 echo "Running prettier for react-native javascript files \n"
 prettier --write . --ignore-path ./.eslintignore
 
 echo "Running ktlint for react-native android files \n"
 cd android
-ktlint -F **/*.kt
+ktlint -F **/*.kt || exit 1
 cd ..
 
 echo "Running swift-format for react-native ios files \n"
 cd ios
-swift-format format -i -r ./*.swift --configuration swift-format-config.json
+swift-format format -i -r ./*.swift --configuration swift-format-config.json || exit 1
 cd ..
